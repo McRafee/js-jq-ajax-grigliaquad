4d34b51a6bab578029939d1035e917d2ac141590
Raffaele Riviello <raffaele.riviello@gmail.com>
2020-03-16 19:03:52 +0100

    complete

---
+++ js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+    // *** LOGIC ENGINE *** //
+
+    $(".square").click(function() {
+        var squareClicked = $(this);
+        $.ajax({
+            url: 'https://flynn.boolean.careers/exercises/api/random/int',
+            method: 'GET',
+            success: function(data) {
+                var randomNumber = data.response;
+                console.log(randomNumber); //debug
+                if (randomNumber <= 5) {
+                    $(squareClicked).addClass("yellow");
+                } else {
+                    $(squareClicked).addClass("green");
+                }
+                $(squareClicked).children("p").text(randomNumber);
+            },
+            error: function() {
+                alert('ERROR');
+            }
+        });
+    });
+
+    // *** FUNCTIONS *** //
+
+    // NOT ASYNC FUNCTION
+    // function remoteRandom(){
+    //     var randomNumber = null;
+    //     $.ajax({
+    //         async: false,
+    //         url: 'https://flynn.boolean.careers/exercises/api/random/int',
+    //         method: 'GET',
+    //         success: function (data) {
+    //             randomNumber = data.response;
+    //         },
+    //         error: function () {
+    //             alert('ERROR');
+    //         }
+    //     });
+    //     return randomNumber;
+    // };
+
+
+
+
+
+
+
+
+
+});
